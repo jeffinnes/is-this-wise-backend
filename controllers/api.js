cee725d4bc3190fb241e97b2fc800cec96d33036
Jeff Innes <contact.me@jeffinnes.dev>
2021-03-30 15:11:08 -0700

    Add rating submission logic
    ITW-13

--- controllers/api.js
+++ controllers/api.js
@@ -1,6 +1,28 @@
 const User = require('../models/user');
 const Advice = require('../models/advice');
 
+async function SubmitRating(req, res) {
+  console.log('in SubmitRating');
+  console.log(`user - ${req.user}`);
+  console.log(req.session.passport);
+  console.log(req.body);
+
+  const updateResults = await User.updateOne(
+    { _id: req.session.passport.user },
+    {
+      $push: {
+        ratingHistory: {
+          adviceSlipID: req.body.adviceID,
+          rating: req.body.submittedRating,
+        },
+      },
+    },
+  );
+
+  console.log(updateResults);
+
+  res.status(204).send();
+}
 
 module.exports = {
   SubmitRating,
