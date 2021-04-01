ed118f80b6b73867205691818999c81d39df4a4f
Jeff Innes <contact.me@jeffinnes.dev>
2021-04-01 16:03:41 -0700

    Initial Advice Schema Identified
    ITW-13

--- models/advice.js
+++ models/advice.js
@@ -2,10 +2,14 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-// ToDo finalize this schema
+/**
+ * Dear Future Jeff, Past and Current Jeff have decided
+ * not to save the actual text of the adviceSlip and just retrieve the text
+ * on the front end when needed. This may or may not have been a good decision.
+ */
+
 const adviceEntrySchema = new Schema({
   adviceSlipID: String,
-  adviceText: String,
   timesRatedGood: Number,
   timesRatedBad: Number,
 });
