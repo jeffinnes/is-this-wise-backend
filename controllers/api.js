680d588d245d3ae21bd0204595b481183ac7f2e3
Jeff Innes <contact.me@jeffinnes.dev>
2021-04-01 16:04:36 -0700

    Ratings are saved to their own document and incrementing their rating sums correctly
    ITW-13

--- controllers/api.js
+++ controllers/api.js
@@ -2,12 +2,8 @@ const User = require('../models/user');
 const Advice = require('../models/advice');
 
 async function SubmitRating(req, res) {
-  console.log('in SubmitRating');
-  console.log(`user - ${req.user}`);
-  console.log(req.session.passport);
-  console.log(req.body);
-
-  const updateResults = await User.updateOne(
+  // ToDo A user might have already rated this advice. Better check for it before pushing
+  await User.updateOne(
     { _id: req.session.passport.user },
     {
       $push: {
@@ -19,7 +15,57 @@ async function SubmitRating(req, res) {
     },
   );
 
-  console.log(updateResults);
+  if (req.body.submittedRating === 'good') {
+    // Save the good rating or create a new advice document with first rating
+    await Advice.findOneAndUpdate(
+      { adviceSlipID: req.body.adviceID },
+      { $inc: { timesRatedGood: 1 } },
+    )
+      .then((result) => {
+        if (!result) {
+          new Advice({
+            adviceSlipID: req.body.adviceID,
+            timesRatedGood: 1,
+            timesRatedBad: 0,
+          })
+            .save()
+            .then((result) => {
+              console.log(result);
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  } else {
+    // Save the bad rating or create a new advice document with first rating
+    await Advice.findOneAndUpdate(
+      { adviceSlipID: req.body.adviceID },
+      { $inc: { timesRatedBad: 1 } },
+    )
+      .then((result) => {
+        if (!result) {
+          new Advice({
+            adviceSlipID: req.body.adviceID,
+            timesRatedGood: 0,
+            timesRatedBad: 1,
+          })
+            .save()
+            .then((result) => {
+              console.log(result);
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
   res.status(204).send();
 }
