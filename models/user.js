dd010a7a9057be89e3eee408b2b75945693d9d4b
Jeff Innes <contact.me@jeffinnes.dev>
2021-08-13 21:11:35 -0700

    Update user model to reflect the data that actually ended up being used.

--- models/user.js
+++ models/user.js
@@ -3,9 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  username: String,
   name: String,
-  email: String,
   githubId: String,
   googleId: String,
   facebookId: String,
